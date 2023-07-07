2080e2f2f1f9e373ad4acc501f6d5dea9c21f98b
Ramilyushka <ramiliaamineva@yandex.ru>
2023-07-07 21:09:55 +0500

    Верстка ячейки

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func configCell (for cell: ImagesListCell) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    
+    //количество ячеек в секции таблицы
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    //должен возвращать ячейку
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell)
+        
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
