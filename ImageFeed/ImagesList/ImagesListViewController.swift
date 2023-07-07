a8000ceebe2810c881d3d332e0c86f9f2a013934
Ramilyushka <ramiliaamineva@yandex.ru>
2023-07-07 23:03:02 +0500

    Заполнены 20 ячеек картинками и кнопками лайк/дизлайк и дата в метод configCell

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -2,15 +2,41 @@ import UIKit
 
 class ImagesListViewController: UIViewController {
 
+    private let photoNames: [String] = Array(0..<20).map{"\($0)"}
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
     @IBOutlet private var tableView: UITableView!
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+            .lightContent
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
     
-    func configCell (for cell: ImagesListCell) {
+    //заполняем ячейку
+    func configCell (for cell: ImagesListCell, with indexPath: IndexPath) {
         
+        guard let image = UIImage(named: photoNames[indexPath.row]) else { return }
+        
+        cell.cellImageView.image = image
+        
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        if indexPath.row % 2 == 0 {
+            cell.likeButton.setImage(UIImage(named: "like_active"), for: .normal)
+        } else {
+            cell.likeButton.setImage(UIImage(named: "like_no_active"), for: .normal)
+        }
     }
 }
 
@@ -18,7 +44,7 @@ extension ImagesListViewController: UITableViewDataSource {
     
     //количество ячеек в секции таблицы
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photoNames.count
     }
     
     //должен возвращать ячейку
@@ -30,7 +56,7 @@ extension ImagesListViewController: UITableViewDataSource {
             return UITableViewCell()
         }
         
-        configCell(for: imageListCell)
+        configCell(for: imageListCell, with: indexPath)
         
         return imageListCell
     }
@@ -38,5 +64,18 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    //высота ячейки зависит от размеров фото и является динамической
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photoNames[indexPath.row]) else {
+            return 0
+        }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
     }
 }
