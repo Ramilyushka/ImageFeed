cd8f9ec5ccfcc241f0aec3f5255f369f176e6d6c
Ramilyushka <ramiliaamineva@yandex.ru>
2023-07-12 22:13:47 +0500

    Сделано 1. Экран профиля 2. Таб бар переключения между вкладками

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Наиль on 12/07/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginProfileLabel: UILabel!
+    
+    
+}
