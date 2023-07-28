7ee5d954558f8d873c5acb5b44b8ea9598999f64
Ramilyushka <ramiliaamineva@yandex.ru>
2023-07-29 00:42:59 +0500

    Создала класс Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Наиль on 28/07/23.
+//
+
+import Foundation
+
+class Constants {
+    let AccessKey = "zCjCSr8jtSOUzOe1wWE7rO2z-9jBhpF9dkQoRiNsugM"
+    let SecreKey = "62iefqS86uTN7dN18YrvJL25U3EfMt0tzwkcbJI9mzo"
+    let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    let AccessScope = "public+read_user+write_likes"
+    let DefaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
+}
